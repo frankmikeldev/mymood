69949c10009807e7b6f6ea550cd72a8c790cb76a
Frank Mikel <frankmikel84@gmail.com>
2026-03-18 02:08:08 -0700

    add manifest.ts and sw.js for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#7c3aed" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
